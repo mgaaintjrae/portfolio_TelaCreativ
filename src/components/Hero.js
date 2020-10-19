7308d0a158e45bb2d60a9f310063c6b2dec23e4d
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-10-19 17:16:49 +0200

    replace logo and hero-img + about

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react"
+// https://github.com/karl-run/react-dark-theme
 import DarkTheme from 'react-dark-theme'
 import Image from "gatsby-image"
 import { Link } from "gatsby"
@@ -19,13 +20,35 @@ const query = graphql`
 
 const Hero = () => {
   const lightTheme = {
-    background: 'white',
-    text: 'black',
+    clrPrimary5: '#7510f7',
+    clrPrimary9: '#e6ecf8',
+    clrPrimary10:'#e6ecf8',
+    clrGrey1:'hsl(209, 61%, 16%)',
+    clrGrey2:'#343434',
+    clrGrey3: 'hsl(209, 34%, 30%)',
+    clrGrey5: 'hsl(210, 22%, 49%)',
+    clrGrey9: '#e6ecf8',
+    clrGrey10:'hsl(210, 36%, 96%)',
+    clrRedDark: '#141c3a',
+    background: '#e6ecf8',
+    clrWhite: 'white',
+    text: '#141c3a',
   }
   
-  const darkTheme = {
-    background: 'black',
-    text: 'white',
+  const darkTheme = {  
+    clrPrimary5:'#5b005c',
+    clrPrimary9: '#be8dbe',
+    clrPrimary10:'#06062B',
+    clrGrey1:'#f0f0f0',
+    clrGrey2:'#000119',
+    clrGrey3: '#e6e6e6',
+    clrGrey5: '#be8dbe',
+    clrGrey9: '#7510f7',
+    clrGrey10:'#000119',
+    clrRedDark: '#7510f7',
+    background: '#06062B',
+    clrWhite: '#06062B',
+    text: '#e6ecf8',
   }
   const {
     file: {
@@ -42,8 +65,8 @@ const Hero = () => {
         <article className="hero-info">
           <div>
             <div className="underline"></div>
-            <h1>Priscilla Roy </h1>
-            <h4>freelance web & mobile UI/UX designer</h4>
+            <h1>Développeuse Web Freelance</h1>
+            <h2 className="name">Priscilla Roy</h2>
             <Link to="/contact" className="btn">
               Contacte-moi
             </Link>
